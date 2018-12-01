7fcabd39779fb51e788c64a8c0a8862165d371f4
Delyan Angelov <delian66@gmail.com>
2018-12-01 21:03:11 +0200

    Cleanup code of day1/solution.php .

--- day1/solution.php
+++ day1/solution.php
@@ -6,7 +6,7 @@
 //$numbers = explode(", ", "+7, +7, -2, -7, -4");    // 14
 $numbers = explode("\n", file_get_contents("day1/input"));
 $clean_numbers = []; foreach($numbers as $n){ $n = (int) $n; if($n===0)continue; $clean_numbers[] = $n;} $numbers = $clean_numbers;
-$flen = count( $numbers ); $cf = 0; $frequencies = []; foreach($numbers as $n){  $cf += $n;    $frequencies[] = $cf; }
+$flen = count( $numbers ); $cf = 0; foreach($numbers as $n){  $cf += $n;  }
 printf("Current frequency after all input: %d | input size: %d\n", $cf, $flen);
 ////////////////////////////////////////////////////////////////////////////////////////////
 $encounters = [0=>1]; $c=0; $f = 0;
