3152bdfea071742a80b3985e985d4b37188d848f
Delyan Angelov <delian66@gmail.com>
2018-12-02 09:50:45 +0200

    Cleanup read_input/1 implementation to make calls to it more concise.

--- common.php
+++ common.php
@@ -1,8 +1,9 @@
 <?php
 
-function read_input($filename){
+function read_input($filename=''){
+    if($filename==='')$filename=SDIR."/input";
     $clean_lines = []; 
-    $lines = explode("\n", file_get_contents("input"));
+    $lines = explode("\n", file_get_contents($filename));
     foreach($lines as $line){
         if($line==='')continue;
         $clean_lines[] = $line;
