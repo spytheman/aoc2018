8b31c46b659062a5dea2e4b20a41e0f48465b4e0
Delyan Angelov <delian66@gmail.com>
2018-12-02 10:50:57 +0200

    Small optimization of day2/solution.php - exit after finding the first nearest match.

--- day2/solution.php
+++ day2/solution.php
@@ -21,6 +21,6 @@ printf("Twos: %d | Threes: %d | Checksum: %d\n", $c2, $c3, $c2*$c3);
 foreach($lines as $l1){
     foreach($lines as $l2){
         $sames=""; for($i=0;$i<$nline;$i++) if($l1[$i]===$l2[$i])$sames.=$l1[$i];
-        if(strlen($sames)===$nline-1)printf("Found mismatched: %s %s . Sames: %s\n", $l1, $l2, $sames);
+        if(strlen($sames)===$nline-1){ printf("Found mismatched: %s %s . Sames: %s\n", $l1, $l2, $sames); exit(0); }
     }
 }
