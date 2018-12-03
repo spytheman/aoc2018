a5bbb6dcab4971b74a981294d70ec25ea77e4ccb
Delyan Angelov <delian66@gmail.com>
2018-12-03 08:37:11 +0200

    Implement some helper funcs like Amap, Afilter, Areduce, ACountAtLeastX, line2digits ...

--- common.php
+++ common.php
@@ -12,3 +12,16 @@ function read_input($filename=''){
     }
     return $clean_lines;
 }
+
+function Amap(array $a, $f): array { return array_map($f, $a); }
+function Afilter(array $a, $f): array { return array_filter($a, $f); }
+function Areduce(array $a, $f, $init){ return array_reduce($a, $f, $init); }
+function Amax(array $a){ return Areduce($a, max, $a[0]); }
+function Amin(array $a){ return Areduce($a, min, $a[0]); }
+function Asum(array $a){ return Areduce($a, function($a,$b){ return $a+$b;}, 0); }
+function Aprod(array $a){ return Areduce($a, function($a,$b){ return $a*$b;}, 1); }
+function ACountAtLeastX(array $a, int $x): array { return Afilter($a, function($aa) use ($x) { return count($aa)>$x; }); }
+function Akeys(array $a){ return array_keys($a); }
+function Avals(array $a){ return array_values($a); }
+
+function line2digits(string $line): array { $res = []; if(preg_match_all("/\d+/",$line,$b)) $res = $b[0];  return $res; }
