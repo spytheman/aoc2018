6b7724cdaf82c6388b6b185130a07863437cdb23
Delyan Angelov <delian66@gmail.com>
2018-12-04 00:54:16 +0200

    Use line2maskedlines/1 and memorizing the already seen masked lines, to speed up day2/solution.php many times (from O(N^2) to O(k*N) ).

--- day2/solution.php
+++ day2/solution.php
@@ -10,9 +10,15 @@ foreach($lines as $line){
     $c3+=Ahas($hvals,3)?1:0;
 }
 printf("Twos: %d | Threes: %d | Checksum: %d\n", $c2, $c3, $c2*$c3);
-foreach($lines as $l1){
-    foreach($lines as $l2){
-        $sames=""; for($i=0;$i<$nline;$i++) if($l1[$i]===$l2[$i])$sames.=$l1[$i];
-        if(strlen($sames)===$nline-1){ printf("Found mismatched: %s %s . Sames: %s\n", $l1, $l2, $sames); exit(0); }
+$seen=[];
+foreach($lines as $line){
+    $mlines = line2maskedlines( $line );
+    foreach($mlines as $mline){
+        if(Ahas_key($seen, $mline )) { 
+            printf("Found mismatched: %s %s . Sames: %s\n", $line, $seen[ $mline ], noSpace($mline));
+            exit(0);
+        }else{
+            $seen[ $mline ] = $line;
+        }
     }
 }
