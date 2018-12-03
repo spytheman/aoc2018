748bd22afafa6413ada699d8045a3ba7e219f1af
Delyan Angelov <delian66@gmail.com>
2018-12-03 12:24:25 +0200

    Use rectangleEach/5 inside day3/solution.php to simplify it.

--- day3/solution.php
+++ day3/solution.php
@@ -7,15 +7,12 @@ $map=[]; $cids=[];
 foreach($lines as $line){
     list($cid, $topx,$topy, $w,$h) = line2digits($line);
     $cids[$cid]=0;
-    for($x=$topx;$x<$topx+$w;$x++){
-        for($y=$topy;$y<$topy+$h;$y++){
-            $coord="{$x}x{$y}";
-            $map[$coord][]=$cid;
-            $tclaimed = $map[$coord];
-            if(count($tclaimed)>1)
-              foreach($tclaimed as $alreadyclaimed)unset($cids[$alreadyclaimed]);
-        }
-    }
+    rectangleEach($topx, $topy, $w, $h, function($x,$y) use (&$cids, &$map, &$cid){
+        $coord="{$x}x{$y}";
+        $map[$coord][]=$cid;
+        $tclaimed = $map[$coord];
+        if(count($tclaimed)>1) foreach($tclaimed as $alreadyclaimed)unset($cids[$alreadyclaimed]);
+    });
 }
 $omap = ACountAtLeastX($map, 1);
 printf("Overlapping tiles of the map: %d\n",count($omap));
