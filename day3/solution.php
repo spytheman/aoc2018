484f8984ff1c8e6f8211b78fb656359d268a676c
Delyan Angelov <delian66@gmail.com>
2018-12-03 19:10:01 +0200

    Double speed up of day3/solution.php, by avoiding function aliasing redirection, and too much inner loop calling of functions.

--- day3/solution.php
+++ day3/solution.php
@@ -2,18 +2,21 @@
 <?php
 include("common.php");
 $lines = read_input();
-ini_set('memory_limit', '512M');
 $map=[]; $cids=[];
 foreach($lines as $line){
-    list($cid, $topx,$topy, $w,$h) = line2digits($line);
+    list($cid,$topx,$topy,$w,$h)=line2digits($line);
     $cids[$cid]=0;
-    rectangleEach($topx, $topy, $w, $h, function($x,$y) use (&$cids, &$map, &$cid){
-        $coord="{$x}x{$y}";
-        $map[$coord][]=$cid;
-        $tclaimed = $map[$coord];
-        if(count($tclaimed)>1)Aunsetkeys($cids, $tclaimed);
-    });
+    $maxx = $topx + $w;
+    $maxy = $topy + $h;
+    for($x=$topx;$x<$maxx;$x++){
+        for($y=$topy;$y<$maxy;$y++){
+            $coord="{$x}x{$y}";
+            $map[$coord][]=$cid;
+            $tclaimed=$map[$coord];
+            if(count($tclaimed)>1)Aunsetkeys($cids,$tclaimed);
+        }
+    }
 }
-$omap = ACountAtLeastX($map, 1);
+$omap = ACountAtLeastX($map,1);
 printf("Overlapping tiles of the map: %d\n",count($omap));
-printf("Claims with no overlap: %s\n",json_encode(array_keys($cids)));
+printf("Claims with no overlap: %s\n",ve(Akeys($cids)));
