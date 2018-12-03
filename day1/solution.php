30ef9c7579d64d34b2939ce6942db1d7e72b1a3a
Delyan Angelov <delian66@gmail.com>
2018-12-03 12:40:22 +0200

    Use Acontains_key in day1/solution.php .

--- day1/solution.php
+++ day1/solution.php
@@ -1,22 +1,15 @@
 #!/usr/bin/env php
 <?php include("common.php");
 $numbers = read_input();
-//$numbers = explode(", ", "+1, -1");                // 0
-//$numbers = explode(", ", "+1, -2, +3, +1");        // 2
-//$numbers = explode(", ", "+3, +3, +4, -2, -4");    // 10
-//$numbers = explode(", ", "-6, +3, +8, +5, -6");    // 5
-//$numbers = explode(", ", "+7, +7, -2, -7, -4");    // 14
 $flen = count( $numbers ); $cf = Asum($numbers);
 printf("Current frequency after all input: %d | input size: %d\n", $cf, $flen);
 ////////////////////////////////////////////////////////////////////////////////////////////
 $encounters = [0=>1]; $f = 0;
 Arepeat( $numbers, function($n, $c) use (&$encounters, &$f){
     $f += $n;
-    //printf("   c:%6d   n:%6d  f: %6d  \n", $c, $n, $f);
-    if( !array_key_exists($f, $encounters) ){
-        $encounters[$f]=1;
-    }else{
+    if(Acontains_key($encounters, $f)){
         printf("First repeat frequency: %d : c=%d\n", $f, $c);
         return false;
     }
+    $encounters[$f]=1;
 });
