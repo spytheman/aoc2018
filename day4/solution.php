5eb9483d3581eea54849fb09789abb5482881e5c
Delyan Angelov <delian66@gmail.com>
2018-12-04 09:10:42 +0200

    Working solution for part 2 on day 4.

--- day4/solution.php
+++ day4/solution.php
@@ -12,8 +12,9 @@ foreach($lines as $line){
 }
 asort($history); $cguards = Akeys($cguards);
 $cguard = 0;
-$guards=[]; $awakes = []; $asleeps = []; $timetable = [];
+$guards=[]; $awakes = []; $asleeps = []; $timetable = []; $ttminutes = [];
 foreach($cguards as $c){ foreach(range(0, 59) as $m){ $timetable[ $c ][$m] = []; } }
+foreach(range(0, 59) as $m){ foreach($cguards as $c){ $ttminutes[ $m ][$c] = 0; } }
 $old_ctime=0; $cstate = 1;
 foreach($history as $ctime=>list($line, list($y,$mon,$d, $h,$m, $cg))){
     //printf("ctime: %d | %02d:%02d | %6d->%6d | line: %s \n", $ctime, $h,$m, $cg, $cguard, $line);
@@ -40,7 +41,10 @@ foreach($history as $ctime=>list($line, list($y,$mon,$d, $h,$m, $cg))){
                 //printf("sleep at %s - %s\n", $xd, $xm);
             }            
             array_pop($asleepminutes);
-            foreach($asleepminutes as $m) $timetable[ $cguard ][ $m ][] = $xd;
+            foreach($asleepminutes as $m) {
+                $timetable[ $cguard ][ $m ][] = $xd;
+                $ttminutes[ $m ][$cguard] += 1;
+            }
             printf("         #%d | day: %s | asleep minutes: %s\n", $cguard, $xd, ve($asleepminutes));
         }
         $old_ctime = $ctime;
@@ -63,3 +67,19 @@ foreach($timetable[ $mostAsleepGuard ] as $km=>$kx){
 }
 printf("Maximum asleeps for guard %d at minute: %d = %d\n", $mostAsleepGuard, $asleepdays_index, $asleepdays );
 printf("Part 1 answer is: %d\n", $mostAsleepGuard * $asleepdays_index);
+
+$mostasleeptimes_per_minute = 0; $mostasleepguard_per_minute = 0; $mostasleepminute = 0;
+foreach(range(0,59) as $m){
+    $mguards = $ttminutes[ $m ];
+    arsort($mguards);
+    $mguard = Akeys($mguards)[0];
+    $mtimes = Avals($mguards)[0];
+    if($mtimes>$mostasleeptimes_per_minute){
+        $mostasleeptimes_per_minute = $mtimes;
+        $mostasleepguard_per_minute = $mguard;
+        $mostasleepminute = $m;
+    }
+    printf("m: %d | mguard: %5d | $mtimes: %3d | guards: %s\n",$m, $mguard, $mtimes, ve($mguards));
+}
+printf("Most asleep minute: %2d | most asleep guard: %5d | most asleep times: %3d\n", $mostasleepminute, $mostasleepguard_per_minute, $mostasleeptimes_per_minute);
+printf("Part 2 answer is: %d\n", $mostasleepguard_per_minute * $mostasleepminute);
