39ec8edd52b89ccd32b8745ebdcf2a1a2df030bd
Delyan Angelov <delian66@gmail.com>
2018-12-04 13:15:51 +0200

    Golf the day1/solution.php .

--- day1/solution.php
+++ day1/solution.php
@@ -1,16 +1,11 @@
 #!/usr/bin/env php
 <?php 
 include("common.php");
-$numbers = read_input();
-$flen = count( $numbers ); $cf = Asum($numbers);
-printf("Current frequency after all input: %d | input size: %d\n", $cf, $flen);
-////////////////////////////////////////////////////////////////////////////////////////////
-$encounters = [0=>1]; $f = 0;
-Arepeat( $numbers, function($n, $c) use (&$encounters, &$f){
-    $f += $n;
-    if(Ahas_key($encounters, $f)){
-        printf("First repeat frequency: %d : c=%d\n", $f, $c);
-        return false;
-    }
-    $encounters[$f]=1;
+$numbers = read_input(); $cf = Asum($numbers);
+printf("Current frequency after all input: %d \n", $cf);
+
+$seen = [0=>1]; $f = 0;
+Arepeat( $numbers, function($n, $c) use (&$seen, &$f){
+    $f += $n; if(!Ahas_key($seen, $f)){ $seen[$f]=1; return; }
+    printf("First repeat frequency: %d : c=%d\n", $f, $c); exit();
 });
