b532a4ff4de62d60b95f8fb77c7650b38b5545c8
Delyan Angelov <delian66@gmail.com>
2018-12-05 08:25:10 +0200

    Cleanup 5.1 solution.

--- day5/solution.php
+++ day5/solution.php
@@ -2,14 +2,12 @@
 <?php 
 include("common.php");
 $input = join('', read_input());
-$small = range('a','z');
-$BIG = range('A','Z');
-$reactionunits = Amap(array_merge(Azip2($small,$BIG), Azip2($BIG,$small)), function($x){ return join('', $x); });
-$c=0;
-while(true){
+$small = range('a','z'); $BIG = range('A','Z');
+$reactionpairs = Amap(array_merge(Azip2($small,$BIG), Azip2($BIG,$small)), function($x){ return join('', $x); });
+$c=0; while(true){
     $c++;
     $oldinput = $input;
-    $input = str_replace($reactionunits, '', $oldinput);
+    $input = str_replace($reactionpairs, '', $oldinput);
     if(strlen($input) === strlen($oldinput)) break;
 }
-printf("Reactions: %d | Remaining units: %d\n", $c, strlen($input));
+printf("Part 1: reactions: %d | remaining units = answer = %d\n", $c, strlen($input));
