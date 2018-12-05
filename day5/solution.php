0530ebc41ff5df87005a45d98386fe704abe9727
Delyan Angelov <delian66@gmail.com>
2018-12-05 08:30:49 +0200

    Extract fullReaction/2 .

--- day5/solution.php
+++ day5/solution.php
@@ -4,10 +4,14 @@ include("common.php");
 $input = join('', read_input());
 $small = range('a','z'); $BIG = range('A','Z');
 $reactionpairs = Amap(array_merge(Azip2($small,$BIG), Azip2($BIG,$small)), function($x){ return join('', $x); });
-$c=0; while(true){
-    $c++;
-    $oldinput = $input;
-    $input = str_replace($reactionpairs, '', $oldinput);
-    if(strlen($input) === strlen($oldinput)) break;
+function fullReaction(string $input, array $reactionpairs): array {
+    $c=0; while(true){
+        $c++;
+        $oldinput = $input;
+        $input = str_replace($reactionpairs, '', $oldinput);
+        if(strlen($input) === strlen($oldinput)) break;
+    }
+    return [$c, $input];
 }
-printf("Part 1: reactions: %d | remaining units = answer = %d\n", $c, strlen($input));
+$part1 = fullReaction($input, $reactionpairs);
+printf("Part 1: reactions: %d | remaining units = answer = %d\n", $part1[0], strlen($part1[1]));
