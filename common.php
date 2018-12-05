cf889670afaeebe228c5375263923bf4239d00d0
Delyan Angelov <delian66@gmail.com>
2018-12-05 02:13:48 +0200

    Make Asum/1 and Aprod/1 call the builtins array_sum/1 and array_product/1.

--- common.php
+++ common.php
@@ -15,8 +15,8 @@ function Amax(array $a){ return Areduce($a, max, $a[0]); }
 function Amin(array $a){ return Areduce($a, min, $a[0]); }
 function Amax_by_key(array $a){ $k=Amax(Akeys($a)); return $a[$k]; }
 function Amin_by_key(array $a){ $k=Amin(Akeys($a)); return $a[$k]; }
-function Asum(array $a){ return Areduce($a, function($a,$b){ return $a+$b;}, 0); }
-function Aprod(array $a){ return Areduce($a, function($a,$b){ return $a*$b;}, 1); }
+function Asum(array $a){ return array_sum($a); }
+function Aprod(array $a){ return array_product($a); }
 function ACountAtLeastX(array $a, int $x): array { return Afilter($a, function($aa) use ($x) { return count($aa)>$x; }); }
 function Akeys(array $a){ return array_keys($a); }
 function Avals(array $a){ return array_values($a); }
