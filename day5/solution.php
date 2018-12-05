167a31d4144f275ad19930463580ed8ffcb1ae66
Delyan Angelov <delian66@gmail.com>
2018-12-05 08:22:59 +0200

    Solved part 1 of day5.

--- day5/solution.php
+++ day5/solution.php
@@ -1,5 +1,15 @@
 #!/usr/bin/env php
 <?php 
 include("common.php");
-$lines = read_input();
-print_r($lines);
+$input = join('', read_input());
+$small = range('a','z');
+$BIG = range('A','Z');
+$reactionunits = Amap(array_merge(Azip2($small,$BIG), Azip2($BIG,$small)), function($x){ return join('', $x); });
+$c=0;
+while(true){
+    $c++;
+    $oldinput = $input;
+    $input = str_replace($reactionunits, '', $oldinput);
+    if(strlen($input) === strlen($oldinput)) break;
+}
+printf("Reactions: %d | Remaining units: %d\n", $c, strlen($input));
