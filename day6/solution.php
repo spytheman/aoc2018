a8e881e2fee1f1a332d402acc92226919a09b934
Delyan Angelov <delian66@gmail.com>
2018-12-06 09:05:19 +0200

    Day 6 part 2 solved . Some cleanup of not needed stuff.

--- day6/solution.php
+++ day6/solution.php
@@ -4,15 +4,15 @@ include("common.php");
 $lines = read_input();
 $x=[];$y=[]; 
 foreach($lines as $line) sscanf($line, "%d, %d", $x[], $y[]);
-$maxx = Amax($x); $maxy = Amax($y); $n=count($x); $coords = range(0,$n-1); 
-function process($x,$y,$maxx,$maxy,$n,$coords,$borderwidth=0){
-    $g=[0=>[]]; $points=array_fill(0,$n,0);
-    foreach(range(-$borderwidth,$maxx+$borderwidth) as $i){
-        foreach(range(-$borderwidth,$maxy+$borderwidth) as $j){
+$maxx = Amax($x); $maxy = Amax($y); $n=count($x); $coords = range(0,$n-1);
+$d=[0=>[]]; $g=[0=>[]]; $points=array_fill(0,$n,0);
+    foreach(range(0,$maxx) as $i){
+        foreach(range(0,$maxy) as $j){
             @$g[$i][$j]=0;
             $distances = []; 
             foreach($coords as $z) $distances[$z] = abs($i-$x[$z]) + abs($j-$y[$z]);
             $m = Amin($distances); $mk = Akeyof($distances,$m);
+            @$d[$i][$j]=Asum($distances);
             $mn = count(Afilter($distances, function($d) use ($m) { return $m === $d; }));
             //printf("i,j=$i,$j | m:$m | mk:$mk | mn:$mn | distances: %s\n",ve($distances));
             if($mn>1)continue;
@@ -22,9 +22,6 @@ function process($x,$y,$maxx,$maxy,$n,$coords,$borderwidth=0){
         }
         //printf("\n");
     }
-    return [$points,$g];
-}
-[$points,$g] = process($x,$y,$maxx,$maxy,$n,$coords,0);
 $borderpoints=[];
 foreach(range(0,$maxx) as $i) {  $borderpoints[]= $g[$i][0];   $borderpoints[]= $g[$i][$maxy]; }
 foreach(range(0,$maxy) as $j) {  $borderpoints[]= $g[0][$j];   $borderpoints[]= $g[$maxx][$j]; }
@@ -33,3 +30,6 @@ $remainingpoints=Afilter($coords, function($z) use( $borderpoints ) { return !Ah
 $areas=[]; foreach($remainingpoints as $c) $areas[] = $points[$c];
 asort($areas);
 printf("Part 1 answer: the size of the largest area that is not infinite is: %d\n", Amax($areas));
+
+$size=0; foreach(range(0,$maxx) as $i) foreach(range(0,$maxy) as $j) if($d[$i][$j]<10000) $size++;
+printf("Part 2 answer: the size of the region containing all locations which have a total distance to all given coordinates of less than 10000 is: %d \n", $size);
