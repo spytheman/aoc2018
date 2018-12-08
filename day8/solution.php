0488fbd5026ed951e63406d95a1539d4a0ad016c
Delyan Angelov <delian66@gmail.com>
2018-12-08 08:06:58 +0200

    Solve part 2 of day 8 too.

--- day8/solution.php
+++ day8/solution.php
@@ -14,6 +14,7 @@ function newNode(){
             'metadata'=>[],
             'index'=>0,
             'size'=>0,
+            'value'=>0,
     ];
 }
 function readNode(array $digits, int $i){
@@ -33,12 +34,24 @@ function readNode(array $digits, int $i){
     $node['size']=$o;
     $node['children'] = $children;
     $node['metadata'] = $metadata;
+    if($nChildren===0){
+        $node['value'] = Asum($metadata);
+    }else{
+        $s = 0;
+        foreach($metadata as $m){
+            $mi = $m-1; if($mi<0)continue;
+            if(!Ahas_key($children, $mi))continue;
+            $s += $children[$mi]['value'];
+        }
+        $node['value'] = $s;
+    }
     $nodes[]=$node;
     return $node;
 }
 $root = readNode($digits, 0);
 foreach($nodes as $n){
-    printf("Node %s , starts at: %d , size: %d\n", $n['name'], $n['index'], $n['size']);
+    printf("Node %s , starts at: %d , size: %d  | value %d\n", $n['name'], $n['index'], $n['size'], $n['value']);
 }
 $metadataSum = Asum(Amap($nodes, function($n){ return Asum($n['metadata']);}));
 printf("Part 1 answer (the sum of all metadata entries) is: %d\n", $metadataSum);
+printf("Part 2 answer (the value of the root node) is: %d\n", $root['value']);
