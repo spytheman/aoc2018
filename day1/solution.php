016e7949c56c68dfbbf32d72e994ac6fd8dceb4b
Delyan Angelov <delian66@gmail.com>
2018-12-08 08:29:42 +0200

    Improve output of day1/solution.php .

--- day1/solution.php
+++ day1/solution.php
@@ -1,9 +1,9 @@
 #!/usr/bin/env php
 <?php 
 include("common.php");
-$numbers = read_input(); echo "Current frequency after all input: ". Asum($numbers)."\n";
+$numbers = read_input(); printf("Part 1 answer (current frequency after all input) is: %d\n", Asum($numbers));
 $seen = [0=>1]; $f = 0;
 Arepeat( $numbers, function($n, $c) use (&$seen, &$f){
     $f += $n; if(!Ahas_key($seen, $f)){ $seen[$f]=1; return; }
-    printf("First repeat frequency: %d : c=%d\n", $f, $c); exit();
+    printf("Part 2 answer (first repeat frequency) is: %d\n", $f); exit();
 });
