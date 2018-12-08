f27ec552a3a99500913717974c0e6294b91dbd93
Delyan Angelov <delian66@gmail.com>
2018-12-08 08:27:46 +0200

    Improve output of day2/solution.php .

--- day2/solution.php
+++ day2/solution.php
@@ -2,9 +2,9 @@
 <?php include("common.php");
 $lines=read_input();
 $d=[];foreach($lines as $line)Ahistogram_update($d,Akeys(Ahistogram(Avals(line2histogram($line)))));
-printf("2s: %d | 3s: %d | Checksum: %d\n",$d[2],$d[3],$d[2]*$d[3]);
+printf("Part 1 answer (checksum) is: %d\n",$d[2]*$d[3]);
 $seen=[];foreach($lines as $line){
     $mlines=line2maskedlines($line);
     foreach($mlines as $mline){
-        if(Ahas_key($seen,$mline)){printf("Found mismatched: %s %s . Sames: %s\n",$line,$seen[$mline],noSpace($mline));exit(0);}
+        if(Ahas_key($seen,$mline)){printf("Part 2 answer (same letters in single letter mismatch) is: %s\n",$line,$seen[$mline],noSpace($mline));exit(0);}
         $seen[$mline]=$line;}}
