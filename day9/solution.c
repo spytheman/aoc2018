7f92d10d284b8d155b5ee0604399f824c9d3c892
Delyan Angelov <delian66@gmail.com>
2018-12-09 12:38:50 +0200

    Cleanup solution.c (remove commented debugging printf calls, merge lines, cleanup output).

--- day9/solution.c
+++ day9/solution.c
@@ -1,50 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef struct place{
-   long m;
+   int m;
    struct place *prev;
    struct place *next;
 }PLACE;
 
-PLACE *insertAfter(PLACE * z, PLACE * newPlace){
+PLACE *insertAfter(PLACE * z, PLACE * nPlace){
    PLACE *x = z;
    PLACE *y = z->next;
-   //printf("insertAfter x:%p[%d] {NEW:%p[%d]} y:%p[%d]\n", x, x->m, newPlace, newPlace->m,  y,y->m);
-   newPlace->prev = x; newPlace->next = y;
-   x->next = newPlace; y->prev = newPlace;
-   return newPlace;
+   nPlace->prev = x; nPlace->next = y;
+   x->next = nPlace; y->prev = nPlace;
+   return nPlace;
 }
 
-PLACE *new_place(long v){
+PLACE *newPlace(int v){
    PLACE *p = malloc(sizeof(PLACE));
-   p->m = v; p->next = 0; p->prev = 0;
+   p->m = v; p->next = p->prev = p;
    return p;
 }
 
 long game(int np, int nm){
-   //printf("      Starting game with %d players and %d marbles ...\n", np, nm);
    long *players = malloc(np * sizeof(long));
-   PLACE *places = new_place(0);
+   PLACE *places = newPlace(0);
    PLACE *cp = places;
    long p = 1;
-   long c = 0;
-   long nc = 0;
-   long placesLength = 1;
-   //long checkpoint = 1; while(1){ if(checkpoint<nm){ checkpoint*=10;}else{ checkpoint/=10; break; } }
-   cp->next = cp->prev = cp;
+   int  c = 0;
+   int  nc = 0;
+   int  placesLength = 1;
    for (int m = 1; m <= nm; m++){
-      PLACE *newPlace = new_place(m);
-      //if (0 == m % checkpoint){  printf("Placing marble %d.\n", m);  }
+      PLACE *nPlace = newPlace(m);
       if (0 == m % 23){
          PLACE *removed = cp->prev->prev->prev->prev->prev->prev->prev;
          players[p] += m;
          players[p] += removed->m;
-         removed->prev->next = removed->next; removed->next->prev = removed->prev; cp = removed->next; free(removed);
+         removed->prev->next = removed->next; removed->next->prev = removed->prev; cp = removed->next;
          placesLength--;
       }else{
          nc = (c + 2) % placesLength;
-         //printf("c: %d | NC: %d\n", c, nc);
-         cp = insertAfter(cp->next, newPlace);
+         cp = insertAfter(cp->next, nPlace);
          c = nc;
          placesLength++;
       }
@@ -59,6 +54,6 @@ int main(){
    int np, nm;
    while (fgets(line, 1024, stdin)){
       sscanf(line, "%d players; last marble is worth %d points\n", &np, &nm);
-      printf("  --------> np: %3d ; nm: %5d ... Highscore: %10ld\n", np, nm, game(np, nm));
+      printf("Highscore (for %3d players and %5d marbles) is: %10ld\n", np, nm, game(np, nm));
    }
 }
