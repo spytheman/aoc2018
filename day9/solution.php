d392ec1c9785e6f68a3edf662a8e73feca70f706
Delyan Angelov <delian66@gmail.com>
2018-12-09 08:59:49 +0200

    Solution to part 2 day 9 (too slow :-|) .

--- day9/solution.php
+++ day9/solution.php
@@ -2,16 +2,19 @@
 <?php 
 include("common.php");
 $lines =  read_input();
-foreach($lines as $line) line2Highscore($line);
-function line2Highscore($line){
+$nPlayers=0; $nMarbles=0;
+foreach($lines as $line) {
     [$nPlayers, $nMarbles] = line2digits($line);
+    line2Highscore($nPlayers, $nMarbles);
+}
+function line2Highscore($nPlayers, $nMarbles){
+    //printf("Calculating highscore for %3d players and %5d marbles ...\n", $nPlayers, $nMarbles);
     $placed = [0];
     $players = Azeros($nPlayers+1);
     $marbles = range(1, $nMarbles);
-    $c = 0; $p = 1;
+    $c = 0; $p = 1; $placedLength = count($placed);
     foreach ($marbles as $m) {
-        if(0 === ($m % 1000))printf("Placing marble %d.\n", $m);
-        $placedLength = count($placed);
+        if(0 === ($m % 10000))printf("Placing marble %d.\n", $m);
         if (0 === ($m % 23)) {
             $removedIndex = (($placedLength + $c - 7) % $placedLength) + 1;
             $removed = $placed[$removedIndex];
@@ -19,10 +22,12 @@ function line2Highscore($line){
             array_splice($placed, $removedIndex, 1);
             $players[$p] += ($m + $removed);
             $c = $removedIndex - 1;
+            $placedLength--;
         } else {
             $newC = ($c + 2) % $placedLength;
             array_splice($placed, $newC + 1, 0, $m);
             $c = $newC;
+            $placedLength++;
         }
         //printf("p: %2d , c: %2d, m: %2d, placed: %s\n", $p, $c, $m, join(' ', $placed));
         $p++;
@@ -30,6 +35,7 @@ function line2Highscore($line){
     }
     //Aprintv($players, "players"); endl();
     $pv = Amax($players);
-    printf("Part 1 answer (winning score) is: %d, for line: '%s'\n", $pv, $line);
+    printf("Part 1 answer (winning score) is: %d, for nplayers: %d and nmarbles: %d .\n", $pv, $nPlayers, $nMarbles);
     return $pv;
 }
+line2Highscore($nPlayers, $nMarbles * 100);
