b285173830216f77221779164c5256a47cd4466f
Delyan Angelov <delian66@gmail.com>
2018-12-10 19:44:46 +0200

    Improve output of day10 solution.php, by using ░ for the empty places, and ▓ for the filled ones.

--- day10/solution.php
+++ day10/solution.php
@@ -18,9 +18,11 @@ foreach(range(0,11000) as $step){
     if($w>80 || $h>80) continue;
     if($wh>$minwh)break;    
     $minwh = $wh;
-    printf("Seconds: %5d , width: %d , height: %d , xmin: %d, ymin: %d, xmax: %d, ymax: %d, wh: %d\n", 
-           $step, $w, $h, $xmin, $ymin, $xmax, $ymax, $wh);
-    $grid = array_fill(0,$h+1,[]); for($y=0;$y<=$h;$y++) for($x=0;$x<=$w;$x++) @$grid[$y][$x]='.';
-    foreach($xpoints as $k=>$x) $grid[ $ypoints[$k] - $ymin ] [ $x - $xmin ] = '#';
+    printf("Seconds: %5d , width: %d , height: %d , ".
+           "xmin: %d, ymin: %d, xmax: %d, ymax: %d, wh: %d\n", 
+           $step, $w, $h, 
+           $xmin, $ymin, $xmax, $ymax, $wh);
+    $grid = array_fill(0,$h+1,[]); for($y=0;$y<=$h;$y++) for($x=0;$x<=$w;$x++) @$grid[$y][$x]='░';
+    foreach($xpoints as $k=>$x) $grid[ $ypoints[$k] - $ymin ] [ $x - $xmin ] = '▓';
 }
 foreach($grid as $gx) echo join('',$gx)."\n";
