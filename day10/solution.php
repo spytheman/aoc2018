cfcbbdfa7e8b4bcd52f6e776b3beb8aedf240cdd
Delyan Angelov <delian66@gmail.com>
2018-12-10 10:08:55 +0200

    Day 10 solution to part 1 and part 2 .

--- day10/solution.php
+++ day10/solution.php
@@ -2,8 +2,37 @@
 <?php 
 include("common.php");
 $lines = read_input();
+$s=(@$argv[2])>0?$argv[2]:10003;
+$points = [];
 $c=0;
-foreach($lines as $line){
-    printf("%04d: %s\n", $c, $line);
-    $c++;
+foreach($lines as $line) $points[]=line2digits($line);
+/// the constants here are produced by tweaking, just so that the apropriate area of the grid is shown
+/// I knew what to seek for, by abusing excel as a ploter :-)
+$gsize = 600; $mx = 500; $my = 200; $opx = 420; $opy = 50;
+$hgsize = $gsize/2; $sx=$gsize; $sy=$gsize; $cx=$sx/$mx; $cy=$sy/$my;
+while(true){
+    printf("s: %5d | mx: %d, my: %d | cx:%f | cy:%f\n", $s, $mx,$my, $cx,$cy);
+    $grid=Azeros($sx*$sy);
+    foreach($points as &$p){
+        $px = $p[0] + $s*$p[2]; $py = $p[1] + $s*$p[3];
+        $spx = round($hgsize+$cx*($px - $opx));
+        $spy = round($hgsize+$cy*($py - $opy));
+        $grid[$spy*$sx + $spx]=1;
+    }
+    $slines = [];
+    for($y=0;$y<$sy;$y++){
+        $sline = []; $slinefiled = false; 
+        for($x=0;$x<$sx;$x++){
+            $c=$grid[$y*$sx+$x] ? '#' : ' ';
+            $sline[]=$c; 
+            if($c==='#')$slinefiled=true;
+        }
+        if($slinefiled){
+            $slines[$y]=$sline;
+            $line = rtrim(join('', $sline));
+            printf("sy: %5d | line: %s\n", $y, $line);
+        }
+    }
+    $s++;
+    exit();
 }
