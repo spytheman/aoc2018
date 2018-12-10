76c5939a4687163559f52b5d5956a96eaab35518
Delyan Angelov <delian66@gmail.com>
2018-12-10 06:52:46 +0200

    Day 9 : small optimization - tracking current player is not needed, since the marbles are monotonically increasing, so can be converted to player numbers when there is a score.

--- day9/solution.deque.php
+++ day9/solution.deque.php
@@ -10,20 +10,18 @@ foreach($lines as $line){
 function game($label='', int $nPlayers, int $nMarbles){
     // The code here is a nearly direct port of the wonderful Python deque solution at:
     // https://www.reddit.com/r/adventofcode/comments/a4i97s/2018_day_9_solutions/ebepyc7/
-    $player = 0;
     $scores = [];
     $circle = new \Ds\Deque([0]);
     foreach(range(1, $nMarbles) as $m){
         if(0===$m%23){
             $circle->rotate(-7);
             $points = $circle->pop() + $m;
-            @$scores[$player] += $points;
+            @$scores[($m % $nPlayers)] += $points;
             $circle->rotate(1);
         }else{
             $circle->rotate(1);
             $circle->push($m);
         }
-        $player = ($player+1) % $nPlayers;
     }
     printf("%s Highscore (for %5d players and %8d marbles) is: %18d \n", $label, $nPlayers, $nMarbles, Amax($scores));               
 }
