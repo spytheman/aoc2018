91c0f4b411512cb710da52dda1b27ca46ae6bbd4
Delyan Angelov <delian66@gmail.com>
2018-12-12 01:27:01 +0200

    day 11 - cleanup of showGridZone/5 in C variant.

--- common.h
+++ common.h
@@ -5,7 +5,7 @@
 void showGridZone(int topx, int topy, int w, int h, GP( int, grid ) ){
    printf("\n");
    printf("# Grid zone {topx,topy}={%d,%d}, {w,h}={%d,%d}\n", topx, topy, w, h);
-   printf("# --------------------------------------------------\n#");
+   printf("# --------------------------------------------------\n");
    int v=0;
    long ysums=0; 
    long yproducts=1;
@@ -19,10 +19,10 @@ void showGridZone(int topx, int topy, int w, int h, GP( int, grid ) ){
          xs+=v; xp*=v;
          printf("%3d ", v);
       }
-      printf(" | lsum: %8ld | lproduct: %8ld\n#", xs, xp);
+      printf(" | lsum: %8ld | lproduct: %8ld\n", xs, xp);
       ysums+=xs; yproducts*=xp;
    }
-   printf(" --------------------------------------------------\n");
+   printf("# --------------------------------------------------\n");
    printf("# Total sum: %8ld\n", ysums);
    printf("# Total product: %10ld\n", yproducts);
    printf("\n");
