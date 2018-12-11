ff611b5df2f8d1688eca0723ba5b301162e3bd45
Delyan Angelov <delian66@gmail.com>
2018-12-11 23:36:34 +0200

    Speedup day 11 solution, by using a precalculated integral image (https://en.wikipedia.org/wiki/Summed-area_table).

--- day11/solution.php
+++ day11/solution.php
@@ -1,12 +1,9 @@
 #!/usr/bin/env php
 <?php 
 include("common.php");
-printf("Part 1 answer: 21,61\n");
-printf("Part 2 answer: 232,251,12\n");
-exit(); /// The actual solution follows, but is very slow for part 2 ... although it stabilizes after ~20 seconds
 $serial = (int) join('', read_input());
 if($argc>2) $serial = (int) $argv[2];
-$grid = array_fill(1, 300, []);
+$grid = array_fill(0, 301, []); for($y=0;$y<301;$y++)$grid[$y]=array_fill(0,301,0);
 for($y=1;$y<=300;$y++){
     for($x=1;$x<=300;$x++){
         $rid = $x + 10;
@@ -15,24 +12,44 @@ for($y=1;$y<=300;$y++){
         $grid[$y][$x]=$d - 5;
     }
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// NB: This solution now uses a precalculated partial sums/integrated image.
+//     This speeds up the calculation of areas in the grid2tops function below.
+//     The technique is described at https://en.wikipedia.org/wiki/Summed-area_table.
+//     The precalculation here is O(maxsize*maxsize), but is done just *once* for the whole grid 
+//     The time cost is ~50ms in pure PHP, for a grid 300x300. The speedup is over 20x ...
+/////////////////////////////////////////////////////////////////////////////////////////////////
+printf("Precalculating isums for serial: %d ...\n", $serial);
+$isums = $grid; for($y=1;$y<=300;$y++) for($x=1;$x<=300;$x++) $isums[$y][$x] = $isums[$y][$x] + $isums[$y-1][$x] + $isums[$y][$x-1] - $isums[$y-1][$x-1];
+printf("Precalculating isums for serial: %d ... done\n", $serial);
+
 function grid2tops($grid, $maxsize=3){
+    global $isums;
     $tops=[0,0,0,0,0];
     $osum = 0;
     foreach(range(1,$maxsize) as $size){
         for($y=1;$y<=300-$size;$y++){
+            $ys=$y+$size; 
             for($x=1;$x<=300-$size;$x++){
-                $s=0;
-                for($j=0;$j<$size;$j++) for($k=0;$k<$size;$k++) $s += $grid[$y + $j ][$x + $k];
+                $xs=$x+$size;
+                
+                //  Using the precalculated integrated image here is O(constant) :-) ...
+                $s = $isums[$ys][$xs] - $isums[$y][$xs] - $isums[$ys][$x] + $isums[$y][$x];
+                
+                // The naive solution commented below, produces the same, but is O(size*size):
+                // $s=0; for($j=0;$j<$size;$j++) for($k=0;$k<$size;$k++) $s += $grid[$y + $j ][$x + $k];
+                
                 if($tops[0]<$s){
                     $tops[0]=$s;
-                    $tops[1]=$x;
-                    $tops[2]=$y;
+                    $tops[1]=$x+1;
+                    $tops[2]=$y+1;
                     $tops[3]=$size;
                     $tops[4]=$grid[$y][$x];
                 }
             }
         }
-        printf("size: %d, tops: %s\n", $size, ve($tops));
+        //printf("size: %3d, tops: %s\n", $size, ve($tops));
         if($osum>=$tops[0]) break;
         $osum = $tops[0];
     }
@@ -40,10 +57,11 @@ function grid2tops($grid, $maxsize=3){
 }
 
 $tops = grid2tops($grid, 3);
-showGridZone($grid, $tops[1], $tops[2], $tops[3], $tops[3]);
 printf("Part 1 answer: %d,%d\n", $tops[1], $tops[2]);
+showGridZone($grid, $tops[1], $tops[2], $tops[3], $tops[3]);
 printf("\n");
 
 $tops = grid2tops($grid, 300);
-showGridZone($grid, $tops[1], $tops[2], $tops[3], $tops[3]);
 printf("Part 2 answer: %d,%d,%d\n", $tops[1], $tops[2], $tops[3]);
+showGridZone($grid, $tops[1], $tops[2], $tops[3], $tops[3],3);
+printf("\n");
