37f5cbdeaa65323846eb24544988f028b85cc9e1
Delyan Angelov <delian66@gmail.com>
2018-12-13 09:56:21 +0200

    Cleanup day12/solution.php output.

--- day12/solution.php
+++ day12/solution.php
@@ -22,7 +22,7 @@ foreach($lines as $line){
 }
 $nstate = count($state); 
 $g=array_fill(0,MAXY+1,array_fill(0,5*$nstate,0)); $glen = count($g[0]);
-$gcenter = 20;
+$gcenter = $nstate;
 for($i=0;$i<$nstate;$i++)$g[0][$i+$gcenter] = $state[$i];
 printf("FSM   0:%3d, 1:%3d                : %32s\n", Acount($fsm,0), Acount($fsm,1), join('',$fsm));
 printf("Initial state len: %3d            : %s\n",$nstate, join('',$state));
@@ -30,7 +30,7 @@ for($y=1;$y<=MAXY;$y++) for($x=2;$x<$glen-2;$x++){
     $os = 16*$g[$y-1][$x-2] + 8*$g[$y-1][$x-1] + 4*$g[$y-1][$x] + 2*$g[$y-1][$x+1] + 1*$g[$y-1][$x+2];
     $g[$y][$x] = $fsm[ $os ];
 }
-$pots=[]; for($x=0;$x<$glen;$x++){ if($g[MAXY][$x]) $pots[]=$x-$gcenter; }
+$pots=[]; for($x=0;$x<$glen;$x++){ if($g[20][$x]) $pots[]=$x-$gcenter; }
 printf("Part 1 answer: %d\n", Asum($pots));
 
 // Part 2 is based on observations 
@@ -45,7 +45,7 @@ for($y=0;$y<MAXY;$y++){
     for($x=0;$x<$glen;$x++){ if($g[$y][$x]) $pots[]=$x-$gcenter; }
     $cs = Asum($pots);
     $fs = remainingSum($y);
-    printf("Y: %3d | fsum: %5d | sum: %5d | ds: %5d | state: %s\n", $y, $fs, $cs, $cs - $ps, str_replace(['0','1'], [SPACE_EMPTY, SPACE_FILLED], join(' ', $g[$y])));
+    printf("Y: %3d | fsum: %5d | sum: %5d | ds: %5d | state: %s\n", $y, $fs, $cs, $cs - $ps, str_replace(['0','1'], [SPACE_EMPTY, SPACE_FILLED], join('', $g[$y])));
     $ps = $cs;
 }
 $finalrepetitions = 50000000000;
