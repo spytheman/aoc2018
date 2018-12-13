d75b497e50afebb62662aeb65e38af3ecc5630fa
Delyan Angelov <delian66@gmail.com>
2018-12-13 21:34:22 +0200

    Day 13 WIP.

--- day13/solution.php
+++ day13/solution.php
@@ -4,25 +4,27 @@ include("common.php");
 $lines = read_input();
 $maxx = Amax(Amap($lines, function($l){ return strlen($l); })); $maxy = count($lines);
 printf("Longest: x: %d y: %d\n",$maxx, $maxy);
-$cars=[];
-$g=array_fill(0,$maxy,[]);for($y=0;$y<$maxy;$y++)$g[$y]=array_fill(0,$maxx,' ');
-$y=0;
+$tracks=array_fill(0,$maxy,[]);for($y=0;$y<$maxy;$y++)$tracks[$y]=array_fill(0,$maxx,' ');
+$cars=[]; $n=0;
 for($y=0;$y<$maxy;$y++){
     $line = $lines[$y]; $nline = strlen($line);
     for($x=0;$x<$nline;$x++) {
         $c = $line[$x];
-        if($c==='>'||$c==='<'||$c==='v'||$c==='^') $cars[]=[$c,[$x,$y],0];
-        $g[$y][$x]=$c;
+        if($c==='>'||$c==='<'||$c==='v'||$c==='^') {
+            $cars[]=[$c,[$x,$y],0,$n++];
+            if($c==='<'||$c==='>')$c='-';
+            if($c==='v'||$c==='^')$c='|';
+        }
+        $tracks[$y][$x]=$c;
     }
 }
-$tracks=array_fill(0,$maxy,[]); 
-for($y=0;$y<$maxy;$y++) for($x=0;$x<$maxx;$x++) $tracks[$y][$x] = str_replace(['<','>','v','^'], '.', $g[$y][$x]);
-//showGridZone($g,0,0,$maxx, $maxy,1);
-//showGridZone($tracks,0,0,$maxx, $maxy,1);
+function showCars($label='cars', $cars){  foreach($cars as $k=>$c) printf("%s car %2d: %s\n", $label, $k, ve($c)); }
+showCars("X Initial", $cars);
+//showGridZone($tracks,0,0,$maxx, $maxy,1);exit();
 
-function move(array $tracks, array &$cars, int $maxx, int $maxy): bool  {
-    static $move=0; $move++;
-    
+function move(array $tracks, array &$cars, int $maxx, int $maxy): array  {
+    static $move=0; 
+    printf("Move: %d\n", $move);
     $c2v=['>'=>[1,0],'<'=>[-1,0],'v'=>[0,1],'^'=>[0,-1]];
     $cturns=['/^'=>'>','/<'=>'v','/>'=>'^','/v'=>'<','\\^'=>'<','\\<'=>'^','\\>'=>'v','\\v'=>'>'];
     $cturnsLeft =['>'=>'^','^'=>'<','<'=>'v','v'=>'>'];
@@ -30,35 +32,72 @@ function move(array $tracks, array &$cars, int $maxx, int $maxy): bool  {
 
     $g=$tracks;
     $positions=[];
-    foreach($cars as $k=>&$c){
+    $corders = []; foreach($cars as $k=>$c){ $corders[$k]=$c[1][1]*$maxx+$c[1][0]; }
+    asort($corders);
+    //printf("Corders: %s | cars: %s\n", ve($corders), ve($cars));
+    foreach(array_keys($corders) as $k){
+        printf("   moving car: %d\n",$k);
+        $c = &$cars[$k];
         [$cx, $cy] = $c[1]; [$vx, $vy] = $c2v[$c[0]]; [$nx, $ny] = [$cx+$vx, $cy+$vy]; $c[1] = [$nx,$ny]; // update car position
         $mnx = ($nx>=$maxx) ? $maxx-1 : $nx; if($mnx<0)$mnx=0;
         $mny = ($ny>=$maxy) ? $maxy-1 : $ny; if($mny<0)$mny=0;
         $np = $tracks[$mny][$mnx];
-        if($np==='+'){
+        if('+' === $np){
             // on intersections
             switch($c[2] % 3){
-             case 0: $c[0]=$cturnsLeft[$c[0]]; break;
-             case 2: $c[0]=$cturnsRight[$c[0]]; break;
+             case 0: $c[0] = $cturnsLeft[$c[0]]; break;
+             case 2: $c[0] =$cturnsRight[$c[0]]; break;
             }
             $c[2]++;
-        }else{
-            $nc = "{$np}{$c[0]}"; 
-            // update car direction if on a corner
-            if(isset($cturns[$nc])) $c[0] = $cturns[$nc]; 
         }
-        $scoords = sprintf("%3d,%3d", $mnx, $mny);
+        if('\\' === $np || '/' === $np ){
+            // update car direction on corners
+            $nc = "{$np}{$c[0]}";
+            $c[0] = $cturns[$nc];
+        }
+        $scoords = sprintf("%d,%d", $mnx, $mny);
         $positions[$scoords][] = $k;
         $g[$mny][$mnx]=$c[0];
+        if(count($positions[$scoords])>1)$g[$mny][$mnx]='x';
         //printf("car %2d: %s\n", $k, ve($c));
     }
-    printf("Move: %6d | Positions: %s\n", $move, ve($positions));
+    //printf("Move: %6d | cars: %s\n", $move, join(' ', Amap($cars, function($c){ return sprintf("%-22s", ve($c)); })));
+    $crashes=[];
     foreach($positions as $p=>$pcis){
         if(count($pcis)<2)continue;
-        printf("Crash at: %s, between: \n", $p);
-        foreach($pcis as $ci) printf("  crashed car %d: %s\n", $ci, ve($cars[$ci]));
-        return false;
-    }    
-    return true;
+        printf("X   m: %6d, Crash at: %s, between cars %s\n", $move, $p, ve($pcis));
+        foreach($pcis as $ci) {
+            printf("X   m: %6d,     crashed car %d: %s\n", $move, $ci, ve($cars[$ci]));
+            unset($cars[$ci]);
+        }
+        $crashes[]=$p;
+    }
+    if(count($crashes)>0){
+        foreach($crashes as $crash){
+            [$x,$y]=explode(",", $crash);
+            //printf("Crash at %d, %d\n",$x,$y);
+            //showGridZone($g, $x-3, $y-3, 7,7, 1);
+        }
+    }
+    showCars("      Cars after move: {$move}", $cars);
+    $move++;
+    return $crashes;
 }
-while(move($tracks, $cars, $maxx, $maxy));
+//printf("======================= Initial grid: =============================\n"); showGridZone($tracks, 0,0, 0,0, 1);
+
+while(true){
+    $crashes=move($tracks, $cars, $maxx, $maxy);
+    if(count($crashes)>0)break;
+}
+printf("X First crash: %s\n", $crashes[0]);
+
+printf("X -----------------------------------------------------------------------------\n");
+while(count($cars)>2){
+    move($tracks, $cars, $maxx, $maxy);
+}
+
+if(0===count($cars)){
+    printf("X All cars crashed.\n");
+}else{
+    showCars("X Remaining", $cars);
+}
