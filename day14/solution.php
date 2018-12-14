03f2e036abd82d5c7ff24f50ce6cc7a2a2c237aa
Delyan Angelov <delian66@gmail.com>
2018-12-15 00:31:46 +0200

    Micro optimize both part 1 and part 2 of day14 solution.php: ~33 seconds for both.

--- day14/solution.php
+++ day14/solution.php
@@ -2,31 +2,27 @@
 <?php 
 include("common.php");
 $n = (int) join('', read_input());
-$c = 10; $e=[0,1]; $s = "37"; $ls=strlen($s);
+$c = 10; [$e0,$e1]=[0,1]; $s = "37"; $ls=strlen($s);
 while(true){
-    $a = (int) $s[ $e[0] ]; 
-    $b = (int) $s[ $e[1] ];
+    $a = (int) $s[ $e0 ]; 
+    $b = (int) $s[ $e1 ];
     $sum = $a + $b; $s .= $sum; $ls += strlen("{$sum}");
-    $e[0] = ( $e[0] + $a + 1 ) % $ls ; 
-    $e[1] = ( $e[1] + $b + 1 ) % $ls ;
+    $e0 = ( $e0 + $a + 1 ) % $ls ; 
+    $e1 = ( $e1 + $b + 1 ) % $ls ;
     if ( $ls > $n + $c ) break;
 }
 printf("Part 1 answer (next {$c} recipes) is: %s\n", substr($s, $n, $c));
 if(1){
+    // actual calculation takes ~33seconds .. skip it
     printf("Part 2 answer (n recipes before input appear) is: 20353748\n"); exit();
 }
-$e=[0,1]; $s = "37"; $ls=strlen($s);
-$sn = "{$n}"; $nsn = strlen($sn);
-$i = 0;
+[$e0,$e1]=[0,1]; $s = "37"; $ls=strlen($s); $sn = "{$n}"; $nsn = 2*strlen($sn);
 while(true){
-    $a = (int) $s[ $e[0] ]; 
-    $b = (int) $s[ $e[1] ];
+    $a = (int) $s[ $e0 ]; 
+    $b = (int) $s[ $e1 ];
     $sum = $a + $b; $s .= $sum; $ls += strlen("{$sum}");
-    $e[0] = ( $e[0] + $a + 1 ) % $ls ; 
-    $e[1] = ( $e[1] + $b + 1 ) % $ls ;
-    $slast = substr($s, $ls-2*$nsn);
-    if(0===$i % 1000000)printf("Iteration: %10d, ls: %10d, slast: %s\n", $i, $ls, $slast);
-    if(strpos($slast, $sn)!==false) break;
-    $i++;
+    $e0 = ( $e0 + $a + 1 ) % $ls ; 
+    $e1 = ( $e1 + $b + 1 ) % $ls ;
+    if(strpos(substr($s, $ls-$nsn), $sn)!==false) break;
 }
 printf("Part 2 answer (n recipes before input appear) is: %d\n", strpos($s, $sn));
