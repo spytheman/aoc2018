e721fb21187933e32f8118daa566f81035abc101
Delyan Angelov <delian66@gmail.com>
2018-12-14 22:40:52 +0200

    Make day14/solution.php as simple as possible, by eliminating dead code and function calls in the main loop.

--- day14/solution.php
+++ day14/solution.php
@@ -8,21 +8,14 @@ $elves=[0,1];
 $initialrecipes = "37";
 printf("N: %d | Initial recipes: %s\n",$n,$initialrecipes);
 $recipes = $initialrecipes; $lastrecipe=strlen($recipes);
-function showState($i, $elves, $recipes, $nrecipes){
-    printf("%4d | Elves: %-10s | Recipes (%5d): %s\n", $i, ve($elves), $nrecipes, $recipes);
-}
 $i=0; while(true){
-    //showState($i, $elves, $recipes, $lastrecipe);
-    $er=[]; foreach($elves as $ee) $er[$ee] = (int) $recipes[$ee]; $s = Asum($er);
-    $recipes.=$s;$lastrecipe+=strlen("{$s}");
-    foreach($elves as &$e){
-        $eforward = $recipes[$e] + 1;
-        $e = ($e + $eforward) % $lastrecipe;
-    }
-    if($lastrecipe > $nstart + $howmanywanted) break;
-    if(0 === $i % 100000)printf("Iteration: %8d | lastrecipe: %10d\n",$i, $lastrecipe);
+    $a = (int) $recipes[ $elves[0] ]; 
+    $b = (int) $recipes[ $elves[1] ];
+    $s = $a + $b; $recipes .= $s; $lastrecipe += strlen("{$s}");
+    $elves[0] = ( $elves[0] + $a + 1 ) % $lastrecipe ;
+    $elves[1] = ( $elves[1] + $b + 1 ) % $lastrecipe ;
+    if ( $lastrecipe > $nstart + $howmanywanted ) break;
     $i++;
 }
-//showState($i, $elves, $recipes, $lastrecipe);
 $wanted = substr($recipes, $nstart, $howmanywanted);
 printf("Wanted %d after %d: %s\n", $howmanywanted, $nstart, $wanted);
