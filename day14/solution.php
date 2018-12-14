96d12807b50900854e080c7f0958f492cace6340
Delyan Angelov <delian66@gmail.com>
2018-12-14 22:57:34 +0200

    Day 14 - shorten part 1 solution to its bare essentials.

--- day14/solution.php
+++ day14/solution.php
@@ -2,20 +2,13 @@
 <?php 
 include("common.php");
 $n = (int) join('', read_input());
-$nstart = $n;
-$howmanywanted = 10;
-$elves=[0,1];
-$initialrecipes = "37";
-printf("N: %d | Initial recipes: %s\n",$n,$initialrecipes);
-$recipes = $initialrecipes; $lastrecipe=strlen($recipes);
-$i=0; while(true){
-    $a = (int) $recipes[ $elves[0] ]; 
-    $b = (int) $recipes[ $elves[1] ];
-    $s = $a + $b; $recipes .= $s; $lastrecipe += strlen("{$s}");
-    $elves[0] = ( $elves[0] + $a + 1 ) % $lastrecipe ;
-    $elves[1] = ( $elves[1] + $b + 1 ) % $lastrecipe ;
-    if ( $lastrecipe > $nstart + $howmanywanted ) break;
-    $i++;
+$c = 10; $e=[0,1]; $s = "37"; $ls=strlen($s);
+while(true){
+    $a = (int) $s[ $e[0] ]; 
+    $b = (int) $s[ $e[1] ];
+    $sum = $a + $b; $s .= $sum; $ls += strlen("{$sum}");
+    $e[0] = ( $e[0] + $a + 1 ) % $ls ; 
+    $e[1] = ( $e[1] + $b + 1 ) % $ls ;
+    if ( $ls > $n + $c ) break;
 }
-$wanted = substr($recipes, $nstart, $howmanywanted);
-printf("Wanted %d after %d: %s\n", $howmanywanted, $nstart, $wanted);
+printf("Part 1 answer (next {$c} recipes) is: %s\n", substr($s, $n, $c));
