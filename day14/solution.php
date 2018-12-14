b7dcb5ee11a90c8a8a4a7143a90cffd986413dee
Delyan Angelov <delian66@gmail.com>
2018-12-14 09:07:36 +0200

    Make day14/solution.php 2x faster, by using a string for storing the recipes, instead of a numeric array (since it avoids most conversions).

--- day14/solution.php
+++ day14/solution.php
@@ -5,18 +5,16 @@ $n = (int) join('', read_input());
 $nstart = $n;
 $howmanywanted = 10;
 $elves=[0,1];
-$initialrecipes = [3,7];
-printf("N: %d | Initial recipes: %s\n",$n,ve($initialrecipes));
-$recipes = Azeros($n+2*$howmanywanted); $lastrecipe = 0;
-foreach($initialrecipes as $k=>$x) { $recipes[ $lastrecipe ] = $x; $lastrecipe++; }
+$initialrecipes = "37";
+printf("N: %d | Initial recipes: %s\n",$n,$initialrecipes);
+$recipes = $initialrecipes; $lastrecipe=strlen($recipes);
 function showState($i, $elves, $recipes, $nrecipes){
-    printf("%4d | Elves: %-10s | Recipes (%5d): %s\n", $i, ve($elves), $nrecipes, ve(array_slice($recipes, 0,$nrecipes)));
+    printf("%4d | Elves: %-10s | Recipes (%5d): %s\n", $i, ve($elves), $nrecipes, $recipes);
 }
 $i=0; while(true){
     //showState($i, $elves, $recipes, $lastrecipe);
-    $er=[]; foreach($elves as $ee) $er[$ee] = $recipes[$ee];
-    $esum = Acast2ints(line2array("".Asum($er)));
-    foreach($esum as $d){  $recipes[ $lastrecipe ] = $d; $lastrecipe++; }
+    $er=[]; foreach($elves as $ee) $er[$ee] = (int) $recipes[$ee]; $s = Asum($er);
+    $recipes.=$s;$lastrecipe+=strlen("{$s}");
     foreach($elves as &$e){
         $eforward = $recipes[$e] + 1;
         $e = ($e + $eforward) % $lastrecipe;
@@ -26,5 +24,5 @@ $i=0; while(true){
     $i++;
 }
 //showState($i, $elves, $recipes, $lastrecipe);
-$wanted = Apart($recipes, $nstart, $howmanywanted);
-printf("Wanted %d after %d: %s\n", $howmanywanted, $nstart, join('',$wanted));
+$wanted = substr($recipes, $nstart, $howmanywanted);
+printf("Wanted %d after %d: %s\n", $howmanywanted, $nstart, $wanted);
