f7d2633f03db3eaff4d9ca0caf56fba6fd342dbe
Delyan Angelov <delian66@gmail.com>
2018-12-16 22:25:49 +0200

    Improve cars crash cleanup in day13.

--- day13/solution.php
+++ day13/solution.php
@@ -41,7 +41,9 @@ function move(array $tracks, array &$cars, int $maxx, int $maxy): array  {
                                                 $c1[1][1] === $c2[1][1] && 
                                                 $c1[1][0] <   $c2[1][0]
                                                 ) ? -1 : 1; });    
+    $crashes=[];
     foreach($cars as $k=>&$c){
+        if (!isset($cars[$k])) continue;
         printf("   moving car: %d\n",$k);
         [$cx, $cy] = $c[1]; [$vx, $vy] = $c2v[$c[0]]; [$nx, $ny] = [$cx+$vx, $cy+$vy]; $c[1] = [$nx,$ny]; // update car position
         $mnx = ($nx>=$maxx) ? $maxx-1 : $nx; if($mnx<0)$mnx=0;
@@ -63,24 +65,22 @@ function move(array $tracks, array &$cars, int $maxx, int $maxy): array  {
         $scoords = sprintf("%d,%d", $mnx, $mny);
         $positions[$scoords][] = $k;
         $g[$mny][$mnx]=$c[0];
-        if(count($positions[$scoords])>1)$g[$mny][$mnx]='x';
-        //printf("car %2d: %s\n", $k, ve($c));
+        if(count($positions[$scoords])>1){            
+            $g[$mny][$mnx]='x';
+            $pcis = $positions[$scoords];
+            printf("X   m: %6d, Crash at: %s, between cars %s\n", $move, $scoords, ve($pcis));
+            foreach($pcis as $ci){
+                printf("X   m: %6d,     crashed car %d: %s\n", $move, $ci, ve($cars[$ci]));
+                unset($cars[$ci]);
+            }
+            $crashes[]=$scoords;
+        }
     }
     //printf("Move: %6d | cars: %s\n", $move, join(' ', Amap($cars, function($c){ return sprintf("%-22s", ve($c)); })));
-    $crashes=[];
-    foreach($positions as $p=>$pcis){
-        if(count($pcis)<2)continue;
-        printf("X   m: %6d, Crash at: %s, between cars %s\n", $move, $p, ve($pcis));
-        foreach($pcis as $ci) {
-            printf("X   m: %6d,     crashed car %d: %s\n", $move, $ci, ve($cars[$ci]));
-            unset($cars[$ci]);
-        }
-        $crashes[]=$p;
-    }
-    if(count($crashes)>0){
+    if(count($crashes)){
         foreach($crashes as $crash){
             [$x,$y]=explode(",", $crash);
-            //printf("Crash at %d, %d\n",$x,$y);
+            printf("Move: %6d | crash at %d, %d\n",$move, $x,$y);
             //showGridZone($g, $x-3, $y-3, 7,7, 1);
         }
     }
