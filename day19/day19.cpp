29b7b92d76e5361cd7111ce33997a5185f345f36
Delyan Angelov <delian66@gmail.com>
2018-12-19 13:02:32 +0200

    Day19: indent all with emacs C-cC-q

--- day19/day19.cpp
+++ day19/day19.cpp
@@ -4,20 +4,20 @@
 
 int main(int argc, char **argv)
 {
-    char line[1024];
-    char inputfile[1024] = {"input"};
-    int initial_reg0 = 0;
-    if( argc > 1 ) {
-        for(int i=0;i<argc;i++) printf("Arg: %d = '%s'\n",i, argv[i]);
-        strcpy(inputfile, argv[1]);
-        if( argc > 2 ){
-            initial_reg0 = atoi( argv[2] );
-        }
-    }
-    printf("Input file: '%s' | initial register 0: %d .\n",inputfile, initial_reg0);
-    FILE *f = fopen(inputfile, "r"); while (fgets(line, 1024, f)){
-        printf("LINE: %s",line);
-    }
-    fclose(f);
-    exit(0);
+     char line[1024];
+     char inputfile[1024] = {"input"};
+     int initial_reg0 = 0;
+     if( argc > 1 ) {
+          for(int i=0;i<argc;i++) printf("Arg: %d = '%s'\n",i, argv[i]);
+          strcpy(inputfile, argv[1]);
+          if( argc > 2 ){
+               initial_reg0 = atoi( argv[2] );
+          }
+     }
+     printf("Input file: '%s' | initial register 0: %d .\n",inputfile, initial_reg0);
+     FILE *f = fopen(inputfile, "r"); while (fgets(line, 1024, f)){
+          printf("LINE: %s",line);
+     }
+     fclose(f);
+     exit(0);
 }
