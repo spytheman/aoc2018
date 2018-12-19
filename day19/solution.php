a5ae17e51ded41e3eb8e36fe8b273621c6713675
Delyan Angelov <delian66@gmail.com>
2018-12-19 10:53:29 +0200

    Day19 part 1 draft.

--- day19/solution.php
+++ day19/solution.php
@@ -2,8 +2,58 @@
 <?php 
 include("common.php");
 $lines = read_input();
+$program = [];
 $c=0;
+$instructions = getInstructionSet();
 foreach($lines as $line){
-    printf("%04d: %s\n", $c, $line);
+    //printf("%04d: %s\n", $c, $line);
+    [$cmd,$_rest] = explode(' ', $line);
+    $args = array_pad( line2digits($line), 3, 0);
+    $program[$c] = array_merge([$cmd], $args);
     $c++;
+}
+
+$programsize = count($program)-1; $cpustate = [0,0,0, 0,0,0,  '#ip'=>'ip', 'ip'=>0];
+$c=0; while(true){
+    $ip  = $cpustate[ $cpustate['#ip'] ];
+    if( $ip > $programsize ) {
+        printf("--->CPU at step: %-3d %s | IP: %-4d \n", $c, state2string($cpustate), $ip);
+        printf("--->IP > programsize: %d . Terminating...\n", $programsize);
+        break;
+    }
+    $ins = $program[ $ip ];
+    printf("CPU at step: %-3d %s | IP: %-4d | INS: %15s\n", $c, state2string($cpustate), $ip, ve($ins));
+    $ncpustate = $instructions[ $ins[0] ]( $cpustate, $ins[1], $ins[2], $ins[3] );
+    $cpustate = $ncpustate;
+    $cpustate[ $cpustate['#ip'] ]++;
+    if($c>100)break;
+    $c++;
+}
+
+function state2string($state){
+    $res = [];
+    foreach( $state as $rname=>$rval) $res[]=sprintf("%1s: %-3d", $rname, $rval);
+    return '['.join(',', $res).']';
+}
+
+function getInstructionSet(): array {
+    return [        
+        "addr"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = $reg[ $ia ] + $reg[ $ib ];         return $o; },
+        "addi"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = $reg[ $ia ] + $ib;                 return $o; },
+        "mulr"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = $reg[ $ia ] * $reg[ $ib ];         return $o; },
+        "muli"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = $reg[ $ia ] * $ib;                 return $o; },
+        "banr"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = $reg[ $ia ] & $reg[ $ib ];         return $o; },
+        "bani"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = $reg[ $ia ] & $ib;                 return $o; },
+        "borr"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = $reg[ $ia ] | $reg[ $ib ];         return $o; },
+        "bori"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = $reg[ $ia ] | $ib;                 return $o; },
+        "setr"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = $reg[ $ia ];                       return $o; },
+        "seti"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = $ia;                               return $o; },
+        "gtir"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = ($ia        >  $reg[$ib]) ? 1 : 0; return $o; },
+        "gtri"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = ($reg[$ia]  >  $ib      ) ? 1 : 0; return $o; },
+        "gtrr"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = ($reg[$ia]  >  $reg[$ib]) ? 1 : 0; return $o; },
+        "eqir"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = ($ia       === $reg[$ib]) ? 1 : 0; return $o; },
+        "eqri"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = ($reg[$ia] === $ib      ) ? 1 : 0; return $o; },
+        "eqrr"=>function($reg,$ia,$ib,$ic){ $o=$reg; $o[$ic] = ($reg[$ia] === $reg[$ib]) ? 1 : 0; return $o; },
+        "#ip" =>function($reg,$ia,$ib,$ic){ $o=$reg; $o['#ip'] = $ia;                             return $o; },
+    ];
 }
