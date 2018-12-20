fe550c1578948d329caa92f97cd3bee8e7dc1ab3
Delyan Angelov <delian66@gmail.com>
2018-12-20 17:58:43 +0200

    Day 20 - draft of (regexp -> lexems -> AST) complete.

--- day20/solution.php
+++ day20/solution.php
@@ -1,63 +1,108 @@
 #!/usr/bin/env php
 <?php 
 include("common.php");
-$regexp = str_replace(['^', '$'], '', join('', read_input())); 
+$regexp = str_replace(['^', '$'], '', join('', read_input()));
+$hregexp = md5($regexp);
 $nregexp = strlen($regexp); 
-printf("Regexp length: %d\n", $nregexp);
-$routes = generateRoutesGivenRegexp($regexp);
-$c = 0; foreach($routes as $r){
-    printf("Route: %5d is: '%s' .\n", $c, $r);
-    $c++;
+printf("Regexp length: %d hash: %32s\n", $nregexp, $hregexp);
+$lexems = regexp2lexems($regexp);
+$ptree = lexems2parsetree($lexems);
+printf("Ptree: %s\n", ve($ptree));
+printTree($ptree);
+function printTree(array $ptree, $level=0){
+    $levelpad = str_pad(' ', 4*$level, ' ');
+    foreach($ptree as $n){
+        if(is_array($n)){
+            printf("{$levelpad} [\n");
+            printTree($n, $level+1);
+            printf("{$levelpad} ]\n");
+        }else{
+            printf("{$levelpad} %s \n", $n);
+        }
+    }
 }
-
-function generateRoutesGivenRegexp(string $regexp, $startindex=0, $level=0): array {
-    $levelpad = str_pad('', $level*10, ' ');
-    printf("{$levelpad} generateRoutesGivenRegexp startindex: %d , level: %d, regexp: %s \n", $startindex, $level, $regexp);
-    $routes = [];
-    $chars = str_split($regexp, 1); $endIndex = count($chars);
-    $curbranch = '';
-    for($i=$startindex;$i<$endIndex;$i++){
-        $c = $chars[$i]; 
-        $nc = ''; if($i + 1 < $endIndex) $nc = $chars[$i+1];
-        printf("{$levelpad} Char %5d: %s\n", $i, $c);
+/////////////////////////////////////////////////////////////////////////////////
+function regexp2lexems(string $regexp=''): array {
+    //printf("regexp2lexems regexp: %s\n", $regexp);
+    $res = [];
+    $nregexp=strlen($regexp);
+    $token = ''; $blevel = 0;
+    for($i=0;$i<$nregexp;$i++){
+        $c = $regexp[$i];
         switch($c){
+         case 'N':;
+         case 'E':;
+         case 'S':;
+         case 'W':{
+             $token.=$c;
+             break;
+         }
          case '(':{
-             $foundb=0; $j=$i; while(true){
-                 if($j>$endIndex){
-                     printf("{$levelpad} Level: %d | Startindex: %d | Syntax error for ( at %d - this bracket has no matching ) .\n", $level, $startindex, $i);
-                     printf("{$levelpad} Regexp: %s \n", $regexp);
-                     exit(0); 
+             if($token!=='')$res[]="_{$token}"; 
+             $res[]="{$c}{$blevel}";
+             $token='';
+             $blevel++;
+             break;
+         }
+         case '|':{
+             if($token!=='')$res[]="_{$token}"; 
+             $res[]=$c; 
+             $token=''; 
+             break;
+         }
+         case ')':{
+             $blevel--;
+             if($token!=='')$res[]="_{$token}"; 
+             $res[]="{$c}{$blevel}";
+             $token='';
+             break;             
+         }
+        }
+    }
+    if($token!=='')$res[]="_{$token}";
+    return $res;
+}
+function lexems2parsetree(array $lexems, $level=0): array {
+    $levelpad = str_pad(' ', $level*5, ' ');
+    //printf("{$levelpad} lexems2parsetree level: %2d, lexems: %s\n", $level, ve($lexems));
+    $res = [];
+    $nlexems = count($lexems);
+    for($i=0;$i<$nlexems;$i++){
+        $lexem = $lexems[$i];        
+        switch($lexem[0]){
+         case '_':{
+             $res[]=$lexem;
+             break;
+         }
+         case '|':{
+             if( $i+1 == $nlexems) {
+                 $res[]='e';
+             }
+             break;
+         }
+         case '(':{
+             $found = false; $needed=$lexem; $needed[0]=')'; $optionlexems=[];
+             for($j=$i+1;$j<$nlexems;$j++){
+                 $olexem = $lexems[$j];
+                 if( $lexems[$j] === $needed ){
+                     $found = true;
+                     break;
                  }
-                 if($chars[$j] === '(')$foundb++; if($chars[$j] === ')')$foundb--; 
-                 if($foundb===0) break;
-                 $j++;
+                 $optionlexems[]=$olexem;
              }
-             $subregexp = substr($regexp, $i+1, $j - 1 - $i );
-             $subroutes = generateRoutesGivenRegexp($subregexp, 0, $level+1);
-             printf("{$levelpad} Subroutes at level %d for subregexp: '%s' | curbranch:'%s'\n", $level, $subregexp, $curbranch);
-             foreach($subroutes as $r) {
-                 printf("{$levelpad}   route: '%s'\n", $r);
-                 $routes[]= $curbranch . $r;
+             if(!$found){
+                 printf("{$levelpad} Syntax error after '%s' . The bracket was not properly closed.\n", ve($res));
+                 exit(1);
              }
-             $curbranch='';
+             $res[]=lexems2parsetree( $optionlexems, $level+1 );
              $i=$j;
              break;
          }
-         case ')':break;
-         case '|':{
-             printf("I: $i , c: '$c' , nc: '$nc' , curbranch: '%s'\n", $curbranch);
-             $routes[]=$curbranch; $curbranch='';
-             if($nc === '') {
-                 $routes[] = '';
-             }
-             break;
-         }
-         default:{
-             $curbranch.=$c;
+         case ')':{
+             printf("{$levelpad} found end of options\n");
              break;
          }
         }
     }
-    if($curbranch!=='')$routes[]=$curbranch;
-    return $routes;
+    return $res;
 }
