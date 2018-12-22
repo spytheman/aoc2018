9ac2f4a4761dd55dc98e47712b531711b6d2ffc1
Delyan Angelov <delian66@gmail.com>
2018-12-22 07:37:43 +0200

    Day 22: Found the total risk (answer to part 1).

--- day22/solution.php
+++ day22/solution.php
@@ -7,9 +7,8 @@ foreach($lines as $line){
     if(strpos($line, 'target:')===0) sscanf($line, 'target: %d, %d', $tx, $ty);
 }
 printf("Depth: %4d, Target: x:%4d y:%4d\n", $depth, $tx, $ty);
-$ag  = A2Dnew($tx,$ty);
-$agi = A2Dnew($tx,$ty);
-$age = A2Dnew($tx,$ty);
+$ag  = A2Dnew($tx,$ty); $agi = A2Dnew($tx,$ty); $age = A2Dnew($tx,$ty);
+$sumrisk = 0;
 for($y=0;$y<=$ty;$y++){
     for($x=0;$x<=$tx;$x++){
         if( ($tx === $x && $ty === $y) || (0 === $x && 0 === $y) ) $gi = 0;
@@ -17,10 +16,12 @@ for($y=0;$y<=$ty;$y++){
         else if(0 === $x )$gi = $y * 48271;
         else $gi = $age[$y][$x-1] * $age[$y-1][$x];
         $ge = ($gi + $depth) % 20183;
-        $ag[$y][$x]  = [0=>'.', 1=>'=', 2=>'|'][$ge % 3];
+        $ag[$y][$x]  = $t = [0=>'.', 1=>'=', 2=>'|'][ $risk = $ge % 3 ];
         $agi[$y][$x] = $gi;
         $age[$y][$x] = $ge;
+        $sumrisk += $risk;
     }
 }
 $ag[0][0]='M'; $ag[$ty][$tx]='T';
 showGridZone($ag, 0,0, $tx+1, $ty+1, 1);
+printf("Total risk for the area: %10d\n", $sumrisk);
