91d815d976826baec12585670879919dce8e3da9
Delyan Angelov <delian66@gmail.com>
2018-12-22 07:10:08 +0200

    Day 22: read input.

--- day22/solution.php
+++ day22/solution.php
@@ -1,9 +1,9 @@
 #!/usr/bin/env php
 <?php 
 include("common.php");
-$lines = read_input();
-$c=0;
+$lines = read_input(); $depth  = 0; $tx=0; $ty=0;
 foreach($lines as $line){
-    printf("%04d: %s\n", $c, $line);
-    $c++;
+    if(strpos($line, 'depth:')===0) sscanf($line, 'depth: %d', $depth);
+    if(strpos($line, 'target:')===0) sscanf($line, 'target: %d, %d', $tx, $ty);
 }
+var_dump($depth, $tx,$ty);
