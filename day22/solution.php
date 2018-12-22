1dd848d2b724f49685003b24afc05eb06c70d7e7
Delyan Angelov <delian66@gmail.com>
2018-12-22 07:17:13 +0200

    Day 22: add an empty grid.

--- day22/solution.php
+++ day22/solution.php
@@ -6,4 +6,5 @@ foreach($lines as $line){
     if(strpos($line, 'depth:')===0) sscanf($line, 'depth: %d', $depth);
     if(strpos($line, 'target:')===0) sscanf($line, 'target: %d, %d', $tx, $ty);
 }
-var_dump($depth, $tx,$ty);
+printf("Depth: %4d, Target: x:%4d y:%4d\n", $depth, $tx, $ty);
+$g = A2Dnew($tx,$ty); showGridZone($g,0,0,$tx+1,$ty+1,1);
