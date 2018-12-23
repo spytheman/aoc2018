58db3fb18df3b28a81a643f3419bf2cd31c9180f
Delyan Angelov <delian66@gmail.com>
2018-12-23 07:17:48 +0200

    Day 23: add finding the maximum radius/range .

--- day23/solution.php
+++ day23/solution.php
@@ -4,3 +4,7 @@ include("common.php");
 $lines = read_input();
 $digits = Amap($lines, function($line){ return line2digits($line); });
 Amap($digits, function($x){ printf("digits: %s\n", ve($x)); });
+$radiuses = Amap($digits, function($x){ return $x[3];});
+$maxradius = Amax($radiuses);
+var_dump($radiuses,$maxradius);
+
