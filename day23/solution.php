3e632b66b191b6cae539a0e87edc23a91be60bb2
Delyan Angelov <delian66@gmail.com>
2018-12-23 07:15:12 +0200

    Day 23: Add printing for validation.

--- day23/solution.php
+++ day23/solution.php
@@ -3,4 +3,4 @@
 include("common.php");
 $lines = read_input();
 $digits = Amap($lines, function($line){ return line2digits($line); });
-var_dump($digits);
+Amap($digits, function($x){ printf("digits: %s\n", ve($x)); });
