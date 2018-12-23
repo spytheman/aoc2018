18029acc1a20fe20c1b0a8772bf0498bab578c61
Delyan Angelov <delian66@gmail.com>
2018-12-23 07:42:35 +0200

    Day 23: part 1 solved. The checker is wrong. Fuck it.

--- day23/solution.php
+++ day23/solution.php
@@ -1,10 +1,22 @@
 #!/usr/bin/env php
 <?php 
 include("common.php");
-$lines = read_input();
-$digits = Amap($lines, function($line){ return line2digits($line); });
-Amap($digits, function($x){ printf("digits: %s\n", ve($x)); });
-$radiuses = Amap($digits, function($x){ return $x[3];});
-$maxradius = Amax($radiuses);
-var_dump($radiuses,$maxradius);
 
+$robots = Amap(read_input(), function($line){ return line2digits($line); });
+printRobots("Ordinary", $robots);
+
+$sortedrobots = $robots;
+usort($sortedrobots, function($a, $b){ return $a[3]<=>$a[3]; });
+printRobots("Sorted", $sortedrobots);
+
+$otherrobots = $sortedrobots;
+$maxrobot = array_shift($otherrobots);
+printRobots("Maximum", [$maxrobot]);
+printRobots("Other", $otherrobots);
+
+$inrangerobots = Afilter($robots, function($x) use ( $maxrobot ):bool { return manhatanDistance($x, $maxrobot) <= $maxrobot[3]; });
+printRobots("Inrange robot",$inrangerobots);
+printf("Robots in range: %d\n", count($inrangerobots));
+
+function printRobots(string $name, array $robots){ Amap($robots, function($x) use ($name) { printf("%s robot: %s\n", $name, ve($x)); }); }
+function manhatanDistance(array $a, array $b): int { return abs($a[0]-$b[0]) + abs($a[1]-$b[1]) + abs($a[2]-$b[2]) ; }
