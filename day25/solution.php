49858f82c2f42921f23fc5141dc348a87704af72
Delyan Angelov <delian66@gmail.com>
2018-12-25 15:46:18 +0200

    Day 25: cleanup code for part 1.

--- day25/solution.php
+++ day25/solution.php
@@ -5,7 +5,7 @@ $lines = read_input();
 $allConstelations=Amap($lines, function($line){ return [line2digits($line)]; });
 $comparisons = 0; while(true){
     $allConstelations = array_values($allConstelations); $nc = count($allConstelations);
-    printf("comparisons so far: %10d | nConstelations: %10d \n", $comparisons, $nc);
+    if(0 === $comparisons || 0===$nc%50) printf("comparisons so far: %10d | nConstelations: %10d \n", $comparisons, $nc);
     for($i=0;$i<$nc;$i++){
         $c1=@$allConstelations[$i];
         if(!$c1)continue;
@@ -20,21 +20,25 @@ $comparisons = 0; while(true){
             }
         }
     }
+    printf(">>>>>> Final comparisons: %10d | nConstelations: %10d \n", $comparisons, $nc);
     break;
 }
 $allConstelations = array_values($allConstelations);
 foreach($allConstelations as $k=>$c) printf("Constelation %5d = %s\n", $k, ve($c));
 printf("Part 1 answer (number of constelations) is: %d\n", count($allConstelations));
 /////////////////////////////////////////////////////////////////////
-function point2string(array $p){ return ve($p); }
+function manh($p1,$p2){return abs($p1[0]-$p2[0])+abs($p1[1]-$p2[1])+abs($p1[2]-$p2[2])+abs($p1[3]-$p2[3]);}
+function pointInConstelation(array $p, array $c){
+    if(count($c)===0) return true; 
+    foreach($c as $cp) if(manh($p,$cp)<=3) return true; 
+}
 function canConstelationsMerge(array $c1, array $c2){
     static $canMergeCache = [];
-    $c1k = ve($c1); 
-    $c2k = ve($c2); 
-    $k="{$c1k}|{$c2k}";
+    $k = ve([$c1, $c2]);
     if(!isset($canMergeCache[$k])) $canMergeCache[$k]=_canConstelationsMerge($c1,$c2);
     return $canMergeCache[$k];
 }
-function _canConstelationsMerge(array $c1, array $c2){ foreach($c1 as $p1) if(pointInConstelation($p1, $c2)) return true; return false; }
-function pointInConstelation(array $p, array $c){ if(count($c)===0) return true; foreach($c as $cp) if(manh($p,$cp)<=3) return true; }
-function manh($p1,$p2){return abs($p1[0]-$p2[0])+abs($p1[1]-$p2[1])+abs($p1[2]-$p2[2])+abs($p1[3]-$p2[3]);}
+function _canConstelationsMerge(array $c1, array $c2){ 
+    foreach($c1 as $p1) if(pointInConstelation($p1, $c2)) return true; 
+    return false; 
+}
